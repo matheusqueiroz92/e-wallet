d0f3a29166fc78a1d965d81abc8daf9370e21e45
Matheus Queiroz <matheus_giga@hotmail.com>
2022-07-29 03:32:40 -0300

    Finalizando o primeiro requisito

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,100 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom';
+import { addUser } from '../redux/actions';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      username: '',
+      password: '',
+      disableButton: true,
+      redirect: false,
+    };
+  }
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({ [name]: value }, this.validateInputs);
+  }
+
+  validateInputs = () => {
+    const SIX = 6;
+    const MENOS_UM = -1;
+    const { username, password } = this.state;
+    if (username.search(/\S+@\S+\.\S+/) !== MENOS_UM && password.length >= SIX) {
+      this.setState({
+        disableButton: false,
+      });
+    } else {
+      this.setState({
+        disableButton: true,
+      });
+    }
+  }
+
+  sendAction = () => {
+    const { username } = this.state;
+    const { addUserDispatch } = this.props;
+    this.setState({ redirect: true });
+    addUserDispatch(username);
+  }
+
   render() {
-    return <div>Login</div>;
+    const { username, password, disableButton, redirect } = this.state;
+    return (
+      <div>
+        { redirect && <Redirect from="/" to="/carteira" />}
+        <div className="title-login">Login</div>
+        <form className="form-login">
+          <label htmlFor="username">
+            Usuário
+            <input
+              type="text"
+              onChange={ this.handleChange }
+              value={ username }
+              name="username"
+              id="username"
+              data-testid="email-input"
+            />
+          </label>
+          <label htmlFor="password">
+            Senha
+            <input
+              type="password"
+              onChange={ this.handleChange }
+              value={ password }
+              name="password"
+              id="password"
+              data-testid="password-input"
+            />
+          </label>
+        </form>
+        <button
+          type="submit"
+          disabled={ disableButton }
+          name="login-button"
+          id="login-button"
+          onClick={ this.sendAction }
+        >
+          Entrar
+        </button>
+      </div>
+    );
   }
 }
 
-export default Login;
+Login.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+  addUserDispatch: PropTypes.func.isRequired,
+};
+
+const mapDispatchToProps = (dispatch) => ({
+  addUserDispatch: (payload) => dispatch(addUser(payload)),
+});
+
+export default connect(null, mapDispatchToProps)(Login);
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -4,3 +4,10 @@
 // Configure os seus reducers.
 // ATENÇÃO: você obrigatoriamente tem que utilizar as chaves "user" e "wallet" no seu estado global
 // Criando o reducer
+import { combineReducers } from 'redux';
+import user from './user';
+import wallet from './wallet';
+
+const rootReducer = combineReducers({ user, wallet });
+
+export default rootReducer;
